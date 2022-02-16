eb86c119f6d3145f57e588857ff4b240c499742b
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-02-16 21:34:50 +0530

    displaying the winner by updating status

--- src/index.js
+++ src/index.js
@@ -37,13 +37,18 @@ class Board extends React.Component {
       );
     }
   
-    render() 
-    {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    render() {
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
-      return (
-        <div>
-          <div className="status">{status}</div>
+    return (
+      <div>
+        <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
